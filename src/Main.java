db29e647208e8c6ee8e971bad0c7fed533a40c9b
Maxim Shvagerus <mshvagerus@gmail.com>
2023-12-12 19:35:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         public static void main(String[] args) {
 
             int eaters = 5; // сколько людей будут есть
-
             int water = 3000; // миллилитров воды
             int potatoes = 5; // картофелин
             int chicken = 6; // куриных бёдер
